14a0bf51bc6f6f05f6d477d69849b2f777f178ef
Sabih Hasan <hasans@eu.hasbro.com>
2021-06-14 22:57:31 +0100

    typo fix

--- model_v1.1.py
+++ model_v1.1.py
@@ -8,21 +8,21 @@ def predict(current_balance: float, regular_income: float, predict_months: int) 
 if __name__ == "__main__":
     print(
         f"Current Balance: £500", 
-        f"Regular Savings: £500",
+        f"Regular Income: £500",
         f"Predicted Balance (1 month): £{predict(500, 500, 1)}",
         sep='\n',
         end='\n\n'
     )
     print(
         f"Current Balance: £500", 
-        f"Regular Savings: £500",
+        f"Regular Income: £500",
         f"Predicted Balance (3 month): £{predict(500, 500, 3)}",
         sep='\n',
         end='\n\n'
     )
     print(
         f"Current Balance: £500", 
-        f"Regular Savings: £500",
+        f"Regular Income: £500",
         f"Predicted Balance (6 month): £{predict(500, 500, 6)}",
         sep='\n',
         end='\n\n'
